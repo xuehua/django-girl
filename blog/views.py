0604e6ee48a0f4cd08d474981707065ef3ca0565
xuehua <xuehua@gmail.com>
2019-12-26 12:22:10 -0800

    Added page to create/edit blog.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -13,5 +13,15 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post':post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    print ("post_new is called!\n")
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
