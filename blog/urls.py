96e8b24f44acf9cd07caf504f68c18356595b253
xuehua <xuehua@gmail.com>
2019-12-26 10:39:02 -0800

    Added a post form.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('',  views.post_list, name='post_list'),
     path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post_new/', views.post_new, name='post_new'),
 ]
