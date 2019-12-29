4af773f87e2de6d93edfc47d41ca5715702f12f1
xuehua <xuehua@gmail.com>
2019-12-28 17:23:41 -0800

    Added post_edit page for blog editing.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('',  views.post_list, name='post_list'),
     path('post/<int:pk>', views.post_detail, name='post_detail'),
     path('post_new/', views.post_new, name='post_new'),
+    path('post/post_edit/<int:pk>', views.post_edit, name='post_edit'),
 ]
