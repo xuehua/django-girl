0e3baeb2cafb174af04ae321489bfc66cfa0acf7
xuehua <xuehua@gmail.com>
2020-01-02 21:01:33 -0800

    Added support for post deletion.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/post_edit/<int:pk>', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name="post_draft_list"),
     path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
 ]
