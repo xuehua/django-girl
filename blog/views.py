b2a9b038f5e02731d3599a30e6b22c55682491ff
xuehua <xuehua@gmail.com>
2020-04-30 20:45:41 -0700

    Added pagination support for posts.

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,26 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')    
+#     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')    
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+
+    try:
+        page_posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_posts = paginator.page(1)
+    except EmptyPage:
+        page_posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post_list.html', {'page':page_number, 'posts': page_posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
