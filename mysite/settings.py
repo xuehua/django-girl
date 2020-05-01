cc8dec1cbb38da634bfec40b58a34bd89f8128df
xuehua <xuehua@gmail.com>
2020-04-30 21:49:01 -0700

    Update settings to use sqlite database for pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,14 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
+
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -93,7 +94,7 @@ DATABASES = {
         'PORT': 5432,
     },   
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
